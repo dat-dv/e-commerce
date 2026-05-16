701b8c50010d777587c608e1a91c4a7ac5fec892
datdoan <datdoan.dev@gmail.com>
2026-05-16 19:36:55 +0700

    chore: optimize tsup build for watch mode and update backend development start scripts

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
+export default defineConfig((options) => ({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   outDir: "dist",
   dts: true,
-  clean: true,
-});
+  clean: !options.watch,
+}));
