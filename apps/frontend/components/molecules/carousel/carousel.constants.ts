48cdc7add0ea8b5ba5ad292f35b48116fced8868
datdoan <datdoan.dev@gmail.com>
2026-05-20 22:11:19 +0700

    style: update product carousel item sizing classes to use calculated basis values

--- apps/frontend/components/molecules/carousel/carousel.constants.ts
+++ apps/frontend/components/molecules/carousel/carousel.constants.ts
@@ -1,5 +1,5 @@
 export const PRODUCT_CAROUSEL_ITEM_CLASS =
-  "flex-[0_0_82%] sm:flex-[0_0_40%] md:flex-[0_0_30%] lg:flex-[0_0_22%] xl:flex-[0_0_20%]";
+  "grow-0 shrink-0 basis-[calc((100%_-_0.75rem)/2)] sm:basis-[calc((100%_-_2rem)/3)] md:basis-[calc((100%_-_3rem)/4)] lg:basis-[calc((100%_-_6rem)/5)]";
 
 export const CATEGORY_CAROUSEL_ITEM_CLASS =
   "flex-[0_0_68%] sm:flex-[0_0_45%] md:flex-[0_0_33.33%] lg:flex-[0_0_20%]";
