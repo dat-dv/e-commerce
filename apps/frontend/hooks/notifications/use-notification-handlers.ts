a82f50203a65e9f4a31d84e9e232b8c97c80c25d
datdoan <datdoan.dev@gmail.com>
2026-05-25 14:02:52 +0700

    refactor: update notification event handlers to use FirebaseServiceWorkerMessagePayload and disable toast display by default

--- apps/frontend/hooks/notifications/use-notification-handlers.ts
+++ apps/frontend/hooks/notifications/use-notification-handlers.ts
@@ -1,7 +1,4 @@
-import {
-  FirebaseServiceWorkerMessagePayload,
-  NotificationEventData,
-} from "@/components/molecules/providers/notification-setup/notification-listener.types.ts";
+import { FirebaseServiceWorkerMessagePayload } from "@/components/molecules/providers/notification-setup/notification-listener.types.ts";
 import { toast } from "@/components/ui/toast";
 import { TOAST_KEYS } from "@/constants/toast.constant";
 import { useUnreadCount } from "@/hooks/notifications/use-unread-count";
@@ -16,12 +13,16 @@ export const useNotificationHandlers = () => {
     (data?: FirebaseServiceWorkerMessagePayload) => void
   > = useMemo(
     () => ({
-      [ENotificationClientEvent.CHANGED]: (data?: NotificationEventData) => {
+      [ENotificationClientEvent.CHANGED]: (
+        data?: FirebaseServiceWorkerMessagePayload,
+      ) => {
         console.log("ENotificationClientEvent.CHANGED", JSON.stringify(data));
         loadUnreadCount();
 
+        // TODO: Handle show toast based on user preference
+        const showToast = false;
         const notiInfo = data?.notification;
-        if (notiInfo?.title) {
+        if (showToast && notiInfo?.title) {
           const toastId = data?.data?.orderId
             ? TOAST_KEYS.ORDER_PLACE(data.data.orderId)
             : undefined;
@@ -32,7 +33,9 @@ export const useNotificationHandlers = () => {
           });
         }
       },
-      [ENotificationClientEvent.REFRESH]: (data?: NotificationEventData) => {
+      [ENotificationClientEvent.REFRESH]: (
+        data?: FirebaseServiceWorkerMessagePayload,
+      ) => {
         console.log("ENotificationClientEvent.REFRESH", data);
         loadUnreadCount();
       },
