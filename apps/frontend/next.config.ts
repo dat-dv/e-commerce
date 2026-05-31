0b2894811fe8501d63f2e1c3769b7ae8ad9fde89
datdoan <datdoan.dev@gmail.com>
2026-05-31 15:56:59 +0700

    chore: migrate admin to nginx deployment and frontend to next.js standalone mode

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  output: "standalone",
   reactStrictMode: !true,
   transpilePackages: ["@ecommerce/shared", "@ecommerce/ui"],
   images: {
